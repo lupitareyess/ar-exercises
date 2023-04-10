ed11e856b8f0fcc00c35dde09b8ba4fc59109867
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-04-10 15:43:14 -0400

    Finish exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+store = Store.new(name: "Testing", annual_revenue: 20000, mens_apparel: false, womens_apparel: false)
+if store.save
+  puts "Successfully saved store:"
+else
+  puts "Failed to save store:"
+  store.errors.full_messages.each do |msg|
+    puts "- #{msg}"
+  end
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :carry_mens_or_womens_apparel
+
+  def carry_mens_or_womens_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:base, "Store must carry womens or mens apparel, at minimum.")
+    end
+  end
 end
