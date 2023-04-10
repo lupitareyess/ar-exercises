7296e290afd1b0918a4c741db42d7d2088d77e9a
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-04-10 14:38:40 -0400

    Finish exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Lupita", last_name: "Reyes", hourly_rate: 60)
+@store1.employees.create(first_name: "Daniel", last_name: "Reyes", hourly_rate: 25)
+@store1.employees.create(first_name: "Rory", last_name: "Rett", hourly_rate: 70)
+@store2.employees.create(first_name: "Dory", last_name: "Nemo", hourly_rate: 80)
+@store2.employees.create(first_name: "Captain", last_name: "America", hourly_rate: 30)
+@store2.employees.create(first_name: "Iron", last_name: "Man", hourly_rate: 90)
